deabb9b192ff42a4b3ae1c6dcf533b4aff543783
eug-vs <eug-vs@keemail.me>
2020-06-21 13:51:20 +0300

    chore: init npm

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { default as User, Gender } from './types/User';
-export { default as Poll, ImageData } from './types/Poll';
-
---
+++ types/index.ts
@@ -0,0 +1,3 @@
+export { default as User, Gender } from './User';
+export { default as Poll, ImageData } from './Poll';
+
