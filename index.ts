3b4991d150a7db05c01516a088e563a8a01f80d9
eug-vs <eug-vs@keemail.me>
2020-06-24 00:22:18 +0300

    fix: add / before folder name

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { default as User, Gender } from '.types/User';
+export { default as User, Gender } from './types/User';
 export { default as Poll, ImageData } from './types/Poll';
 export { default as Which } from './types/Which';
 
